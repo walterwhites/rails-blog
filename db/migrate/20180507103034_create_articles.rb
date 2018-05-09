a5e68ea598f8466401b06f9fc40734fff7fcbcc9
flo <hopemagie@gmail.com>
2018-05-09 22:52:30 +0200

    change db

--- db/migrate/20180507103034_create_articles.rb
+++ db/migrate/20180507103034_create_articles.rb
@@ -3,5 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles
       add_column :articles, :title, :string
       add_column :articles, :content, :text
+      add_column :articles, :categorid, :integer
+      add_index :articles, :category_id
  end
 end
--- db/migrate/20180507104221_add_articles.rb
+++ db/migrate/20180507104221_add_articles.rb
@@ -1,7 +1,19 @@
 class AddArticles < ActiveRecord::Migration[5.2]
   def self.up
-      Articles.new(:title => "yesterday", :content => "chanson des beatles").save
-      Articles.new(:title => "yesterday", :content => "chanson des beatles").save
-      Articles.new(:title => "yesterday", :content => "chanson des beatles").save
+      Article.create :title => "article 1", :content => "description", :category_id => 1
+      Article.create :title => "article 2", :content => "description", :category_id => 0
+      Article.create :title => "article 3", :content => "description", :category_id => 0
+      Article.create :title => "article 4", :content => "description", :category_id => 1
+      Article.create :title => "article 5", :content => "description", :category_id => 3
+      Article.create :title => "article 6", :content => "description", :category_id => 3
+      Article.create :title => "article 7", :content => "description", :category_id => 3
+      Article.create :title => "article 8", :content => "description", :category_id => 1
+      Article.create :title => "article 9", :content => "description", :category_id => 1
+      Article.create :title => "article 10", :content => "description", :category_id => 1
+      Article.create :title => "article 11", :content => "description", :category_id => 0
+      Article.create :title => "article 12", :content => "description", :category_id => 0
+      Article.create :title => "article 13", :content => "description", :category_id => 3
+      Article.create :title => "article 14", :content => "description", :category_id => 3
+      Article.create :title => "article 15", :content => "description", :category_id => 1
     end
 end
